1af75636c1f3c3f9765f4a976171ca39c4d77d81
Eugene Chernov <vaertion@mail.ru>
2023-09-27 20:45:29 +0300

    Initial Commit

---
+++ IAmRich_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich_app
+//
+//  Created by Eugene Bekker on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
